2d26c1d6cb33571415e3330f693b360f906f92cc
Malik Olivier Boussejra <m.boussejra@fj.ics.keio.ac.jp>
2016-04-29 00:25:00 +0900

    Get something that works. Seems like there is a blit bug

--- times-table.py
+++ times-table.py
@@ -9,13 +9,14 @@ parser.add_argument('N', metavar='N', default=100, nargs='?', type=int,
                     "Values between 10 and 1000 should work well, default is 100.")
 parser.add_argument('-t', '--times', nargs=2, default=[2, 2], type=float,
                     help="Min and max of the 'times' factor.")
+parser.add_argument('-s', '--speed', type=float, default=10, help="The higher the faster. 100 is very fast, 1 is quite slow.")
+parser.add_argument('-w', '--write', type=str, help="Write the output in a mp4 file.")
 
 args = parser.parse_args()
 
 import matplotlib.pyplot as plt
 import matplotlib.collections as mc
 import numpy as np
-
 
 ax = plt.axes()
 ax.set_aspect('equal')
@@ -24,6 +25,17 @@ ax.set_ylim([-1.1, 1.1])
 
 class TimesTable:
     def __init__(self, times=2, N=100):
+        self._N = N
+        self._line_collection = mc.LineCollection(TimesTable._get_segments(times, N))
+
+    def add_to_axis(self, ax, **kwargs):
+        ax.add_collection(self._line_collection, kwargs)
+
+    def set_times(self, times):
+        self._line_collection.set_segments(TimesTable._get_segments(times, self._N))
+
+    @staticmethod
+    def _get_segments(times, N):
         segments = np.zeros(N, list)
         for i in range(N):
             x0 = np.cos(i*2*np.pi/N)
@@ -31,15 +43,39 @@ class TimesTable:
             x1 = np.cos(times*i*2*np.pi/N)
             y1 = np.sin(times*i*2*np.pi/N)
             segments[i] = [(x0, y0), (x1, y1)]
-        self._line_collection = mc.LineCollection(segments)
+        return segments
+
+class AnimatedTimesTable(TimesTable):
+    def __init__(self, times=[2, 100], N=100, speed=0.01, interval=40):
+        self._times = times
+        TimesTable.__init__(self, times[0], N)
+        self._interval = interval
+        self._speed = speed
+
+    def _init(self):
+        return self._line_collection,
+    def _animate(self, t):
+        self.set_times(t)
+        return self._line_collection,
 
     def add_to_axis(self, ax, **kwargs):
-        ax.add_collection(self._line_collection, kwargs)
+        TimesTable.add_to_axis(self, ax, **kwargs)
+        arange = np.arange(self._times[0], self._times[1], (self._times[1] - self._times[0]) * self._speed)
+        ani = animation.FuncAnimation(ax.figure, self._animate, arange,
+                                      init_func=self._init, interval=self._interval, blit=True)
 
-if args.times[0] == args.times[1]:
+
+
+if args.times[0] == args.times[1] or args.speed == 0:
     times_table = TimesTable(args.times[0], args.N)
     times_table.add_to_axis(ax)
 
     plt.show()
 else:
-    raise "Not implemented"
+    from matplotlib import animation
+    times_table = AnimatedTimesTable(args.times, args.N, args.speed/1000)
+    times_table.add_to_axis(ax)
+    if args.write is None:
+        plt.show()
+    else:
+        raise "Not implemented"
